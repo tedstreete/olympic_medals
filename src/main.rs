0473b4b11b52d0af4f0e6e5db83873ed7670e0a1
ted <dev@streete.org>
2024-07-31 09:45:08 -0700

    Minor updates to html template

--- src/main.rs
+++ src/main.rs
@@ -31,43 +31,44 @@ fn main() -> Result<(), Box<dyn Error>> {
 fn get_html_table(table_data: &Vec<MedalsRecord>) -> String {
     let mut table = String::new();
     for country_medals in table_data {
-        // table.push_str("    <tr>\n");
-        // table.push_str(&format!("      <td>{}</td>", country_medals.country_code));
-        table.push_str(&format!("      <td>{}</td>", country_medals.country_name));
-        table.push_str(&format!("      <td>{}</td>", dollar(country_medals.gdp)));
+        table.push_str(&format!("<td>{}</td>", country_medals.country_name));
         table.push_str(&format!(
-            "      <td>{}</td>",
-            comma_delim(country_medals.population)
+            "<td>{}</td>",
+            comma_delimited_currency(country_medals.gdp)
         ));
         table.push_str(&format!(
-            "      <td>{}</td>",
-            dollar(country_medals.gdp_per_capita)
+            "<td>{}</td>",
+            comma_delimited_number(country_medals.population)
         ));
         table.push_str(&format!(
-            "      <td>{}</td>",
-            comma_delim(country_medals.gold)
+            "<td>{}</td>",
+            comma_delimited_currency(country_medals.gdp_per_capita)
         ));
         table.push_str(&format!(
-            "      <td>{}</td>",
-            comma_delim(country_medals.silver)
+            "<td>{}</td>",
+            comma_delimited_number(country_medals.gold)
         ));
         table.push_str(&format!(
-            "      <td>{}</td>",
-            comma_delim(country_medals.bronze)
+            "<td>{}</td>",
+            comma_delimited_number(country_medals.silver)
         ));
         table.push_str(&format!(
-            "      <td>{}</td>",
-            comma_delim(country_medals.total)
+            "<td>{}</td>",
+            comma_delimited_number(country_medals.bronze)
         ));
         table.push_str(&format!(
-            "      <td>{}</td>",
-            dollar(country_medals.gdp_per_medal)
+            "<td>{}</td>",
+            comma_delimited_number(country_medals.total)
         ));
         table.push_str(&format!(
-            "      <td>{}</td>",
-            dollar(country_medals.gdp_per_capita_per_medal)
+            "<td>{}</td>",
+            comma_delimited_currency(country_medals.gdp_per_medal)
         ));
-        table.push_str("    </tr>\n");
+        table.push_str(&format!(
+            "<td>{}</td>",
+            comma_delimited_currency(country_medals.gdp_per_capita_per_medal)
+        ));
+        table.push_str("</tr>\n");
     }
     table
 }
@@ -97,15 +98,15 @@ fn save_to_csv(records: &Vec<MedalsRecord>, file_path: &str) -> Result<(), Box<d
     Ok(())
 }
 
-fn comma_delim<T: Into<u64>>(num: T) -> String {
+fn comma_delimited_number<T: Into<u64>>(num: T) -> String {
     let num_u64: u64 = num.into();
     num_u64.to_formatted_string(&Locale::en)
 }
 
-fn dollar<T: Into<u64>>(num: T) -> String {
+fn comma_delimited_currency<T: Into<u64>>(num: T) -> String {
     let num_u64: u64 = num.into();
     let mut currency = String::from("$");
-    let comma_format = comma_delim(num_u64);
+    let comma_format = comma_delimited_number(num_u64);
     currency.push_str(&comma_format);
     currency
 }
