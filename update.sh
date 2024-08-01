68dcdfe567c509e0f80ff6e1e194ba8892f37ac8
ted <dev@streete.org>
2024-08-01 10:11:50 -0700

    Clean-up wrapper script

--- update.sh
+++ update.sh
@@ -4,17 +4,16 @@ MIN_DELAY=$((60 * 60))    # 60 minutes in seconds
 MAX_DELAY=$((90 * 60))    # 90 minutes in seconds
 
 while true; do
-    DELAY=$(RANDOM % (MAX_DELAY - MIN_DELAY + 1) + MIN_DELAY)
-    DELAY_MINUTES=$((DELAY / 60))
-    echo "Waiting for $DELAY_MINUTES minutes before running the application."
-    sleep $DELAY
-    
-    cargo run
+  cargo run
     git add contents.txt
     git add index.html
     git add Medal-Records.csv
     git commit -m "Routine medals update"
     git push
     echo "Update medals count at $(date)"
+    
+    DELAY_MINUTES=$(($RANDOM%($MAX_DELAY-$MIN_DELAY+1)+$MIN_DELAY))
+    echo "Waiting for $DELAY_MINUTES minutes before next medals table update ."
+    sleep $DELAY_MINUTES
 
 done
